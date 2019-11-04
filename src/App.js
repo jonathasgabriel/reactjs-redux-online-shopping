5633a9e7f260838dc8fd27237daea4076f30f42c
jonathas harb <jonathasgabriel05@gmail.com>
2019-11-04 17:45:26 -0300

    adding validatons / react saga to reactotron / toastify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './config/ReactotronConfig';
 
+import { ToastContainer } from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import GlobalStyle from './styles/global';
@@ -15,6 +16,7 @@ function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
+        <ToastContainer autoClose={3000} />
         <GlobalStyle />
         <Header />
         <Routes />
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,13 @@ import createSagaMiddleware from 'redux-saga';
 import rootSaga from './modules/rootSaga';
 import rootReducer from './modules/rootReducer';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+const sagaMiddleware = createSagaMiddleware({
+  sagaMonitor,
+});
 
 const enhancer =
   process.env.NODE_ENV === 'development'
