b5ea15219cad326d39b384651ed19e19fab81ff3
jonathas harb <jonathasgabriel05@gmail.com>
2019-11-04 23:08:26 -0300

    page redirection with within saga

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import React from 'react';
 import './config/ReactotronConfig';
 
 import { ToastContainer } from 'react-toastify';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import GlobalStyle from './styles/global';
 import Header from './component/Header';
 
 import Routes from './routes';
-
+import history from './services/history';
 import store from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <ToastContainer autoClose={3000} />
         <GlobalStyle />
         <Header />
         <Routes />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,9 +1,11 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import api from '../../../services/api';
+import history from '../../../services/history';
+
 import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
   const productExists = yield select(state =>
@@ -23,7 +25,7 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -34,7 +36,28 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSuccess(data));
+
+    history.push('/cart');
   }
 }
 
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) {
+    return;
+  }
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Requested product amount not available in stock');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
